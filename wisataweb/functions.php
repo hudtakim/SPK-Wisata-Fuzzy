65cf8bd1767486c398ace8350721df5c9667df87
Huda Mustakim <47106605+hudtakim@users.noreply.github.com>
2021-07-11 09:30:18 +0700

    Add files via upload

---
+++ wisataweb/functions.php
@@ -0,0 +1,8 @@
+<?php
+	$dbhost = "localhost";
+	$dbuser = "root";
+	$pass = "";
+	$db = "wisata_tegal_db";
+	$conn = mysqli_connect($dbhost, $dbuser, $pass, $db ) or die ("gagal masuk database");
+
+?>
